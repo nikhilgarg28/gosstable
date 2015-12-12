dec2e454c817e41c6fdfa458abdcb7a353d55ecd
Nikhil Garg <nikhilgarg28@gmail.com>
2015-12-12 15:04:49 -0800

    add benchmarks for writing, loading and getting

--- gosstable_test.go
+++ gosstable_test.go
@@ -3,17 +3,20 @@ package gosstable
 import (
 	"bytes"
 	crand "crypto/rand"
+	"fmt"
 	"math/rand"
 	"testing"
 	"time"
 )
 
-func TestWriteGet(t *testing.T) {
+// global variable, to ensure compiler can't mess up with benchmarks
+var result int
+
+func getDictionary(n, min_key_len, max_key_len, min_val_len, max_val_len int) *Dictionary {
 	var d Dictionary
-	size := 10000
-	for i := 0; i < size; i++ {
-		key_len := rand.Intn(1000) + 20
-		val_len := rand.Intn(1000) + 20
+	for i := 0; i < n; i++ {
+		key_len := rand.Intn(max_key_len-min_key_len) + min_key_len
+		val_len := rand.Intn(max_val_len) + min_val_len
 		k := make([]byte, key_len)
 		crand.Read(k)
 
@@ -24,8 +27,13 @@ func TestWriteGet(t *testing.T) {
 		d = append(d, *kv)
 	}
 
-	filename := "test.tbl"
-	Write(filename, &d)
+	return &d
+}
+
+func TestWriteGet(t *testing.T) {
+	d := getDictionary(10000, 20, 1000, 20, 1000)
+	filename := "test.sstb"
+	Write(filename, d)
 
 	// now load the table and see if we can read all the keys back
 	table := New(filename)
@@ -35,7 +43,7 @@ func TestWriteGet(t *testing.T) {
 		time.Sleep(1 * time.Millisecond)
 	}
 
-	for _, kv := range d {
+	for _, kv := range *d {
 		k, v := kv.key, kv.value
 		ret := table.Get(k)
 
@@ -45,18 +53,17 @@ func TestWriteGet(t *testing.T) {
 	}
 
 	// let's try to retrive keys that don't exist
-	for i := 0; i < size; i++ {
-		key_len := rand.Intn(1000) + 20
-		k := make([]byte, key_len)
-		crand.Read(k)
+	d2 := getDictionary(10000, 20, 1000, 20, 1000)
 
+	for _, kv2 := range *d2 {
+		k := kv2.key
 		ret := table.Get(k)
 		if ret != nil {
 			// we found a match in table, when we didn't expect to
 			// before declaring it as an error, let's verify that we didn't
 			// get a random sequence by chance
 			found := false
-			for _, kv := range d {
+			for _, kv := range *d {
 				if bytes.Equal(kv.key, k) {
 					found = true
 					break
@@ -69,3 +76,70 @@ func TestWriteGet(t *testing.T) {
 		}
 	}
 }
+
+func benchmarkWrite(i int, b *testing.B) {
+	d := getDictionary(i, 20, 100, 20, 200)
+	b.ResetTimer()
+
+	filename := "benchmark.sstb"
+
+	for i := 0; i < b.N; i++ {
+		Write(filename, d)
+	}
+}
+
+func BenchmarkWrite10K(b *testing.B)     { benchmarkWrite(10000, b) }
+func BenchmarkWrite100K(b *testing.B)    { benchmarkWrite(100000, b) }
+func BenchmarkWriteMillion(b *testing.B) { benchmarkWrite(1000000, b) }
+
+func benchmarkLoad(i int, b *testing.B) {
+	d := getDictionary(i, 20, 100, 20, 200)
+
+	for i := 0; i < b.N; i++ {
+		// we use a different file each time to bypass file system caching
+		b.StopTimer()
+		filename := fmt.Sprintf("benchmark_%d.sstb", i)
+		Write(filename, d)
+		b.StartTimer()
+		table := New(filename)
+
+		// wait till we read the table
+		for table.Status() == loading {
+			time.Sleep(1 * time.Millisecond)
+		}
+	}
+}
+
+func BenchmarkLoad10K(b *testing.B)     { benchmarkLoad(10000, b) }
+func BenchmarkLoad100K(b *testing.B)    { benchmarkLoad(100000, b) }
+func BenchmarkLoadMillion(b *testing.B) { benchmarkLoad(1000000, b) }
+
+func benchmarkReadHit(s int, b *testing.B) {
+	d := getDictionary(s, 20, 100, 20, 200)
+	filename := "benchmark.sstb"
+	Write(filename, d)
+	table := New(filename)
+	// wait till we read the table
+	for table.Status() == loading {
+		time.Sleep(1 * time.Millisecond)
+	}
+	dict := *d
+
+	indices := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		indices[i] = rand.Intn(s)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		idx := indices[i]
+		key := dict[idx].key
+		v := table.Get(key)
+		result += len(v)
+	}
+}
+
+func BenchmarkReadHit10K(b *testing.B)     { benchmarkReadHit(10000, b) }
+func BenchmarkReadHit100K(b *testing.B)    { benchmarkReadHit(100000, b) }
+func BenchmarkReadHitMillion(b *testing.B) { benchmarkReadHit(1000000, b) }
